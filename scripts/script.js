054d809061aa633796758191e664878dc609cbc1
Alex Valyshev <avalyshev@mail.ru>
2020-06-15 13:15:50 +0300

    fix:Надо исправить ошибки после ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,5 @@
-console.log('Hello!');
 let popup = document.querySelector('.popup');
-let popupClose = popup.querySelector('.popup__image');
+let popupClose = popup.querySelector('.popup__close');
 let popupEditButton = document.querySelector('.profile__editbutton');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
@@ -9,20 +8,20 @@ let nameInput = formElement.querySelector('.popup__text_margin_name');
 let jobInput = formElement.querySelector('.popup__text_margin_job');
 
 function OpenClosePopup() {
-  popup.classList.toggle('popup__opened');
-  // console.log(popup);
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileJob.textContent;
+  popup.classList.toggle('popup_opened');
+  if (popup.classList.contains('popup_opened') === true) {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+  }
 }
-popupEditButton.addEventListener('click', OpenClosePopup);
-popupClose.addEventListener('click', OpenClosePopup);
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  // console.log(nameInput.value);
-  // console.log(jobInput.value);
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
   OpenClosePopup()
 }
+
+popupEditButton.addEventListener('click', OpenClosePopup);
+popupClose.addEventListener('click', OpenClosePopup);
 formElement.addEventListener('submit', formSubmitHandler);
